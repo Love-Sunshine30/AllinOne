153e7f6bd198c62f200e630d5d07e89ac62c7ab3
Love-Sunshine30 <alimran1171@gmail.com>
2025-07-20 06:46:07 +0600

    Fix the server bug

--- server/handler.go
+++ server/handler.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFS(content, "internal/ui/index.html"))
-	tmpl.Execute(w, nil)
-}
-
-func download(w http.ResponseWriter, r *http.Request) {
-	//get the pdf name
-	pdfName := r.URL.Path[len("/download/"):]
-	// read the file
-	data, err := content.ReadFile("internal/pdfs/" + pdfName)
-
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-
-	// Set headers to force download
-	w.Header().Set("Content-Type", "application/pdf")
-	w.Header().Set("Content-Disposition", "attachment; filename=\""+pdfName+"\"")
-	w.Write(data)
+	http.ServeFile(w, r, "internal/ui/index.html")
 }
--- server/main.go
+++ server/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"embed"
 	"fmt"
 	"net/http"
 )
 
-//go:embed internal/*
-var content embed.FS
-
 func main() {
+	fs := http.FileServer(http.Dir("./internal/ui"))
+	http.Handle("/internal/ui/", http.StripPrefix("/internal/ui/", fs))
+
 	http.HandleFunc("/", home)
-	http.HandleFunc("/download/", download)
 
 	fmt.Println("Starting server on port 8080...")
 	http.ListenAndServe(":8080", nil)
